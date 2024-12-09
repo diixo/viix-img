60c24e721319e77677a77e067a3a3c43c501f4d9
apodoba <anatoliy.podoba@dxc.com>
2024-12-09 06:07:48 +0200

    docker-compose.yml: added ssh port + comment

--- install-tools.sh
+++ install-tools.sh
@@ -10,5 +10,3 @@ cp tools/baseimage-docker-nsenter /usr/local/bin/
 mkdir -p /usr/local/share/baseimage-docker
 cp image/services/sshd/keys/insecure_key /usr/local/share/baseimage-docker/
 chmod 644 /usr/local/share/baseimage-docker/insecure_key
-# force set permissions for source key
-#chmod 600 image/services/sshd/keys/insecure_key
